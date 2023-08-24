cb1ead5e1290c4914046cf3ee61abc0487e915b8
Bhavykumar Devani <105499961+bhavy3103@users.noreply.github.com>
2023-08-24 11:04:47 +0530

    Update Main.java

--- src/MainPackage/Main.java
+++ src/MainPackage/Main.java
@@ -519,7 +519,7 @@ public class Main extends Application {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String a[]) {
         launch(args);
     }
 }
